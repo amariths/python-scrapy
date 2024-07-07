4bc19570f014a598fd95f8ea4fb0e33c1255eba3
amariths <amar.mehdi@iths.se>
2024-07-07 21:34:38 +0200

    a

--- python-scrapy/chocolatescraper/chocolatescraper/settings.py
+++ python-scrapy/chocolatescraper/chocolatescraper/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = "chocolatescraper.spiders"
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 25
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -50,9 +50,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "chocolatescraper.middlewares.ChocolatescraperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+  #  "chocolatescraper.middlewares.ChocolatescraperDownloaderMiddleware": 543,
+  'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+  'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
